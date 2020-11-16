289a18e0be766b9a15187c16e0c9dcd6b1ff3b04
UntacticalPenguin <ilja.afonin@gmail.com>
2020-11-16 19:57:13 +0100

    Added paint on Left-click-and-drag function, this created small bug where browser thinks user is trying to move an html element and thus dismantling the mousedown Eventlistener.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let isdrawing = false;
+
 function create_grid(grid_number){
   let container = document.querySelector(".container");
   container.style['grid-template-columns'] = 'repeat(' + grid_number + ', ' + (768/grid_number) + 'px)';
@@ -5,11 +7,24 @@ function create_grid(grid_number){
   for (i = 0; i<((grid_number*grid_number)); i++){
     let element = document.createElement('div');
     element.classList.add('edited_square');
-    element.style.width = (768/grid_number) + 'px'
-    element.style.height = (768/grid_number) + 'px'
-    container.appendChild(element)
+    element.style.width = (768/grid_number) + 'px';
+    element.style.height = (768/grid_number) + 'px';
+    container.appendChild(element);
+    element.addEventListener("mouseover", event =>{
+      if (isdrawing === true){
+        element.style.backgroundColor = 'green';
+      }
+    });
+    element.addEventListener("mousedown", event =>{
+      element.style.backgroundColor = 'green';
+      isdrawing = true;
+    });
+    element.addEventListener("mouseup", event =>{
+      isdrawing = false;
+    });
   }
 }
+
 
 function delete_grid(){
   let container = document.querySelector(".container");
@@ -17,8 +32,8 @@ function delete_grid(){
     container.removeChild(container.lastChild);
   }
 }
-
 btn_list = document.querySelectorAll("p");
+
 for (btn of Array.from(btn_list)){
   btn.addEventListener("click", event => {
     delete_grid();
@@ -32,45 +47,4 @@ for (btn of Array.from(btn_list)){
     event.target.style.backgroundColor = "grey";
   })
 }
-
-/*let btn_list = document.querySelectorAll('p');
-
-for (btn of Array.from(btn_list)){
-  btn.addEventListener("click", event => {
-    for (btn_select of Array.from(document.querySelectorAll('p'))){
-      btn_select.style.backgroundColor = 'white';
-      btn_select.style.color = 'black';
-    }
-    event.target.style.backgroundColor = 'grey';
-    event.target.style.color = 'white';
-    create_grid(parseInt(event.target.textContent));
-    delete_grid();
-  })
-}
-
-function delete_grid(){
-  delete_list = document.querySelectorAll('.edited_square');
-  for (elem of Array.from(delete_list)){
-    elem.remove();
-  }
-}
-
-function create_grid(grid_number){
-  let container = document.querySelector(".container");
-  container.style['grid-template-columns'] = 'repeat(' + grid_number + ', ' + (768/grid_number) + 'px)';
-  container.style['grid-template-rows'] = 'repeat(' + grid_number + ', ' + (768/grid_number) + 'px)';
-  for (i = 0; i<((grid_number*grid_number)); i++){
-    let element = document.createElement('div');
-    element.classList.add('edited_square');
-    element.style.width = (768/grid_number) + 'px'
-    element.style.height = (768/grid_number) + 'px'
-    container.appendChild(element)
-  }
-  square = document.getElementsByClassName('edited_square');
-  square.style.height = (768/grid_number)+'px';
-  square.style.width = (768/grid_number)+'px';
-  existing = true;
-}
-
-*/
 
